d5357068e5e95c1b7270beb72f504e3ab89c81b7
Ernest <dite_malacoda@hotmail.com>
2024-08-03 20:06:24 -0600

    contextapi terminado

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,10 @@
-import { ChangeEvent, useState, MouseEvent, useMemo } from 'react';
+import { ChangeEvent, useState, useMemo, FormEvent } from 'react';
+
+import { useBudget } from '../hooks/useBudget';
 
 export default function BudgetForm() {
 	const [budget, setBudget] = useState(0);
+	const { dispatch } = useBudget();
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setBudget(e.target.valueAsNumber);
@@ -10,12 +13,14 @@ export default function BudgetForm() {
 	// Validando input budget
 	const isValid = useMemo(() => isNaN(budget) || budget <= 0, [budget]);
 
-	const handleSubmit = (e: MouseEvent<HTMLInputElement, globalThis.MouseEvent>) => {
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+
+		dispatch({ type: 'add-budget', payload: { budget } });
 	};
 
 	return (
-		<form className='space-y-5'>
+		<form className='space-y-5' onSubmit={handleSubmit}>
 			<div className='flex flex-col space-y-5'>
 				<label htmlFor='budget' className='text-4xl text-blue-600 font-bold text-center'>
 					Definir presupuesto
@@ -36,7 +41,6 @@ export default function BudgetForm() {
 				type='submit'
 				className='bg-blue-600 hover:bg-blue-700 text-white font-bold uppercase cursor-pointer w-full p-2 disabled:opacity-40'
 				disabled={isValid}
-				onClick={(e) => handleSubmit(e)}
 				value={'Definir presupuesto'}
 			/>
 		</form>
